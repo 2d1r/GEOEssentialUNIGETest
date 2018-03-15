fb6702408c079ac7a2e7288edb6fb45ef0a9618b
Denisa Rodila <denisa_rodila@yahoo.com>
2018-03-15 16:41:17 +0100

    unzip correctly

--- Script/runUNIGE.sh
+++ Script/runUNIGE.sh
@@ -3,6 +3,7 @@
 echo "Hello VLab from UNIGE"
 
 #awk '{print NF}' ../GEOEssentialUNIGEData/unigeinput.txt
-unzip ../Data/wpa.zip
-unzip ../Data/kba.zip
-ogr2ogr merge.shp ../Data/merge.vrt
+cd ../Data
+unzip wpa.zip
+unzip kba.zip
+ogr2ogr merge.shp merge.vrt
